65982f9d2976f2d11d483e81f73646245e49d67f
ab7515 <andrei-octavian.brabete15@imperial.ac.uk>
2017-01-12 15:01:05 +0000

    Defined clause to check if list is sublist of another from a certain position.

--- graph.pl
+++ graph.pl
@@ -43,6 +43,16 @@
 
     % hasdups(?X)
     hasdups([X | T]) :- 
-      member(X, T).
+      member(X, T), hasdups(T).
     
-    prod([X | T], P) :- TODO
+    prod([X], X).
+    prod([X | T], P) :-
+      prod(T, Y),
+      P is Y * X.
+
+    % L2 is a sublist of list L1 starting from pos N
+    % Count positions 1..N
+    contains(L1, L2, N) :-
+      append(YS, L2, L3), append(L3, _1, L1), write(_1),length(YS, M), N is M + 1.
+      
+      
